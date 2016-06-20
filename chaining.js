a08721e06083581ab57f02607ac649c20d156ec1
Thomas J. Shavor <thomasshavor@gmail.com>
2016-06-20 08:46:35 -0500

    fixed code, needs cleanup

--- chaining.js
+++ chaining.js
@@ -7,23 +7,29 @@ var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 // STEP THREE: Multiply each remaining number by 1.5 and then subtract 1.
 // STEP FOUR: Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
-// var chaining= integers.sort(function(first, second){return second-first;})
-// console.log(chaining); 
-
-var chaining= integers.sort().reverse().filter(function(x) {
-	return x <= 19;
+var chaining= integers.sort(function(first, second){return second-first
+})
+.filter(function(x) {
+    return x <= 19
+}).map(function(y) {
+    return (y * 1.5) - 1
+}).reduce(function(a, b) {
+    return a + b;
 });
 
-// .sort(function(first, second){
-// 	return second-first;
-// })
 
-console.log(chaining);
+console.log("after running through the functions, your new number" + " =", chaining); 
 
-// var oneThing = function (thing) {
-// 	return thing <= 19;
-// } 
 
-// var chaining = integers.filter(oneThing);
 
-// console.log(chaining);
+
+// var newArray = integers.sort(function(a, b) {
+//     return b - a
+// }).filter(function(x) {
+//     return x <= 19
+// }).map(function(y) {
+//     return (y * 1.5) - 1
+// }).reduce(function(a, b) {
+//     return a + b;
+// });
+// console.log("after running through the functions, your new number" + " =", newArray);
